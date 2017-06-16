0aa45be997d5ccbff6376d757503d1c18ef7b815
Mattvers <mateusz.grzegorzewicz@gmail.com>
2017-06-16 12:33:33 +0200

    Create a Subscriber model and adding references

---
+++ ServiceSub/ServiceSub/Models/SubscriptionContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ServiceSub.Models
+{
+    public class SubscriptionContext : DbContext
+    {
+        public SubscriptionContext()
+            :base ("ServiceSub")
+        {
+        }
+
+        public DbSet<Subscriber> Subscribers { get; set; }
+    }
+}
