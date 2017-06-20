0271238e3c1f5c11e45b796c2f484e56da673edf
Mattvers <mateusz.grzegorzewicz@gmail.com>
2017-06-20 12:45:18 +0200

    Create Views and Controller for Administrator

---
+++ ServiceSub/ServiceSub/Controllers/AdministratorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ServiceSub.Models;
+
+namespace ServiceSub.Controllers
+{
+    [Authorize]
+    public class AdministratorController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }       
+    }
+}
