0f59501091be9825fe2e434c7e948202b6b3d975
Mattvers <mateusz.grzegorzewicz@gmail.com>
2017-06-20 17:12:55 +0200

    Complete Login Page and Admin rules

--- ServiceSub/ServiceSub/Controllers/AdministratorController.cs
+++ ServiceSub/ServiceSub/Controllers/AdministratorController.cs
@@ -4,16 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ServiceSub.Models;
+using System.Data.Entity;
 
 namespace ServiceSub.Controllers
 {
     [Authorize]
+    [AllowAnonymous]
     public class AdministratorController : Controller
-    {
-        [AllowAnonymous]
+    {       
         public ActionResult Login()
         {
             return View();
-        }       
+        }     
+        
+        [HttpPost]       
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Users u)
+        {
+            // section for post login
+            if(ModelState.IsValid)
+            {
+                using (ServiceSubEntities dc = new ServiceSubEntities())
+                {
+                    var local = dc.Users.Where(a => a.userName.Equals(u.userName) && a.password.Equals(u.password)).FirstOrDefault();
+
+                    if (local != null)
+                    {
+                        Session["LogedUserID"] = local.Id.ToString();
+                        Session["LogedUserFullName"] = local.FullName.ToString();
+                        return RedirectToAction("AfterLogin");
+                    }
+                }
+            }
+            return View(u);
+        }
+
+        public ActionResult AfterLogin()
+        {
+            if (Session["LogedUserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
